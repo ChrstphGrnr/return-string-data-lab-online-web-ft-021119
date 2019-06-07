a8dd9bd11960331eb6820230e64a106dd66fc987
ChrstphGrnr <ChristophGruener@me.com>
2019-06-07 12:04:08 -0600

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,7 @@ class ProductsController < ApplicationController
     def create 
         @product = Product.new(product_params)
         @product.save
-        redirect_to posts_path
+        redirect_to products_path
     end
 
     def description
